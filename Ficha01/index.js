947746cd92c8dd19f4a770424270ec95d52a3045
joaomaria04 <joaomaria.ferrei04@gmail.com>
2023-02-28 18:31:33 +0000

    Update index.js

--- Ficha01/index.js
+++ Ficha01/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+function calculateGrade(gp, gt){
+    var grade = gp * 0.6 + gt * 0.4;
+    if (grade >= 9.5){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+var approval = calculateGrade(12,16);
+calculateGrade(8,8);
+calculateGrade(12,10);
+calculateGrade(11,19);
