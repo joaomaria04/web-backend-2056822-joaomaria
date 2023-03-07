19682e76a8a81151baae9e4fb92e3e5f64f257d8
joaomaria04 <joaomaria.ferrei04@gmail.com>
2023-03-07 17:16:17 +0000

    Update index.js

--- Ficha01/index.js
+++ Ficha01/index.js
@@ -1,13 +1,58 @@
 function calculateGrade(gp, gt){
     var grade = gp * 0.6 + gt * 0.4;
     if (grade >= 9.5){
-        return true;
+        console.log("Aprovado");
     }else{
-        return false;
+        console.log("Reprovado");
     }
 }
 
-var approval = calculateGrade(12,16);
-calculateGrade(8,8);
-calculateGrade(12,10);
-calculateGrade(11,19);
+//calculateGrade(12,16);
+
+
+function numeromes(num_mes) {
+    switch (num_mes) {
+        case 1:
+            console.log("Janeiro"); 
+            break;
+        case 2:
+            console.log("Fevereiro"); 
+            break;
+        case 3:
+            console.log("Março"); 
+            break;
+        case 4:
+            console.log("Abril"); 
+            break;    
+        case 5:
+            console.log("Maio"); 
+            break;  
+        case 6:
+            console.log("Junho"); 
+            break; 
+        case 7:
+            console.log("Julho"); 
+            break; 
+        case 8:
+            console.log("Agosto"); 
+            break; 
+        case 9:
+            console.log("Setembro"); 
+            break; 
+        case 10:
+            console.log("Outubro"); 
+            break;    
+        case 11:
+            console.log("Novembro"); 
+            break;    
+        case 12:
+            console.log("Dezembro"); 
+            break; 
+        default:
+            console.log("Mês inexistente"); 
+            break;
+    }
+
+}
+
+numeromes(1)
