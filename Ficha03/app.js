365e7730c8bcd129ff1c44b521697ab2ea7eb6fa
joaomaria04 <joaomaria.ferrei04@gmail.com>
2023-03-16 11:53:02 +0000

    16/03/2023 11:52

--- Ficha03/app.js
+++ Ficha03/app.js
@@ -1,8 +1,32 @@
-var myLogModule = require('./Log');
-myLogModule.info('Node.js started');
+function started(){
+    console.log("Download STARTED!");
+}
 
-var person = require('./Person.js');
-var person1 = new person('James', 'Bond');
-var person2 = new person('Lebron', 'James');
-console.log(person1.fullName());
-console.log(person2.fullName());
+function update(progress){
+    console.log(progress + "% COMPLETED");
+}
+
+function completed(){
+    console.log("Download COMPLETED!");
+}
+
+function performDownload(fn_s, fn_u, fn_c){
+    fn_s();
+
+    for (i = 0; i <= 100; i++) {
+        fn_u(i);
+    }
+
+    fn_c();
+}
+
+performDownload(started, update, completed); //Estas 3 funções são chamadas de modo a que fn_s, fn_u e fn_c tenham valor de started, update e completed
+
+var ArrayUtils = require('./ArrayUtils.js');
+var array = [1,2,3,4,9];
+var empty = ArrayUtils.isEmpty(array);
+console.log(ArrayUtils.maximum(array));
+console.log(ArrayUtils.minimum(array));
+console.log(ArrayUtils.average(array));
+console.log(ArrayUtils.indexOf(array, 9));
+console.log(ArrayUtils.subArray(array, 1, 3));
