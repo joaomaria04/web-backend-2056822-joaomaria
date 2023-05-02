ec5184878439ca4c4a83cc51a9b0b18b8bc0ab40
joaomaria04 <joaomaria.ferrei04@gmail.com>
2023-05-02 19:22:14 +0100

    Update persons.js

--- Ficha07/routes/persons.js
+++ Ficha07/routes/persons.js
@@ -30,7 +30,20 @@ router.get('/:id', function(req, res, next) {
 
 router.delete('/:id', function(req, res, next) {
     var id = req.params.id; 
-    connection.query("DELETE * FROM persons WHERE id = ?" , id, (err, results, fields) =>{
+    connection.query("DELETE FROM persons WHERE id = ?" , id, (err, results, fields) =>{
+        res.send(results);
+    })
+});
+
+router.get('/:age/:lastname', function(req, res, next) {
+    connection.query("SELECT * FROM persons WHERE age = ? AND lastname = ?" , [req.params.age, req.params.lastname], (err, results, fields) =>{
+        res.send(results);
+    })
+});
+
+router.post('/', function(req, res, next) {
+    var person = req.body;
+    connection.query("INSERT INTO persons SET = ?" , [person], (err, results, fields) =>{
         res.send(results);
     })
 });
